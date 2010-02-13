ffa96a6954a3ef72416de91053527309373a0acb
Sam Newman <sam.newman@gmail.com>
2010-02-13 20:51:44 +0000

    cleanup....

--- src/appengine_clj/test_utils.clj
+++ src/appengine_clj/test_utils.clj
@@ -11,31 +11,3 @@
     (try
       ~@body
       (finally (.tearDown (LocalServiceTestHelperFactory/create))))))
-
-;private final LocalServiceTestHelper helper =
-;        new LocalServiceTestHelper(new LocalDatastoreServiceTestConfig());
-
-;TestLocalServerEnvironment
-;(.setProperty LocalDatastoreService/NO_STORAGE_PROPERTY "true")
-;(defn ds-setup []
-;  (let [proxy-factory (doto (ApiProxyLocalFactory.) )
-;        test-environment (TestLocalServerEnvironment.)
-;        api-proxy     (doto (.create proxy-factory test-environment))]
-;    (ApiProxy/setDelegate api-proxy))
-;  (ApiProxy/setEnvironmentForCurrentThread
-;    (proxy [com.google.apphosting.api.ApiProxy$Environment] []
-;      (getAppId [] "test")
-;      (getVersionId [] "1.0")
-;      (getRequestNamespace [] "")
-;      (getAttributes [] (java.util.HashMap.)))))
-;
-;(defn ds-teardown []
-;  (ApiProxy/clearEnvironmentForCurrentThread)
-;  (.stop (ApiProxy/getDelegate)))
-;
-;(defmacro dstest2 [name & body]
-;  `(test-is/deftest ~name
-;    (ds-setup)
-;    (try
-;      ~@body
-;      (finally (ds-teardown)))))
